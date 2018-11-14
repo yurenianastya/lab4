b41f81029cdcf00666f8c093b0d293d11f667b1e
Nastya <43321431+yurenianastya@users.noreply.github.com>
2018-11-14 18:53:28 +0200

    Structurized code more readibly

--- main.cpp
+++ main.cpp
@@ -6,14 +6,28 @@ int main(){
     Laptop number_1(" Windows ",8,345,3256," Lenovo ",34," N7365er ");
     Laptop number_2(" IoT ",4,3245,553256," HP ",64," T4365er ");
     Laptop number_3(" Linux ",28,44575,243256," Meizu ",54," UIe65er ");
-    cout <<"Model:" << number_2.getModel() << " OS:" << number_2.getOS() << " Firm:" << number_2.getNameOfFirm() << " RAM:" <<
-    number_2.getStorageRAM() << " Diagonal:" << number_2.getDiagonal() << " CPU Gen:" << number_2.getCPUgeneration() << " CPU speed:" << number_2.getSpeedCPU() << "\n";
-    cout <<"Model:" << number_1.getModel() << " OS:" << number_1.getOS() << " Firm:" << number_1.getNameOfFirm() << " RAM:" <<
-         number_1.getStorageRAM() << " Diagonal:" << number_1.getDiagonal() << " CPU Gen" << number_1.getCPUgeneration() << " CPU speed:" <<
-         number_1.getCPUgeneration() << " CPU speed:" << number_1.getSpeedCPU() << "\n";
-    cout << "Model:" << number_3.getModel() << " OS:" << number_3.getOS() << " Firm:" << number_3.getNameOfFirm() << " RAM:" <<
-         number_3.getStorageRAM() << " Diagonal:" << number_3.getDiagonal() << " CPU Gen:" << number_3.getCPUgeneration() << " Cpu Speed:" <<
-         number_3.getCPUgeneration() << " Cpu speed:" << number_3.getSpeedCPU() << "\n";
+    cout << "Model:" << number_2.getModel() << endl;
+    cout << "OS:" << number_2.getOS() << endl;
+    cout << "Firm:" << number_2.getNameOfFirm() << endl;
+    cout << "RAM:" << number_2.getStorageRAM() << endl;
+    cout << "Diagonal:" << number_2.getDiagonal() << endl;
+    cout << "CPU Gen:" << number_2.getCPUgeneration() << endl;
+    cout << "CPU speed:" << number_2.getSpeedCPU() << endl;
+    cout << "Model:" << number_1.getModel() << endl;
+    cout << "OS:" << number_1.getOS() << endl << "\n";
+    cout << "Firm:" << number_1.getNameOfFirm() << endl;
+    cout << "RAM:" << number_1.getStorageRAM() << endl;
+    cout << "Diagonal:" << number_1.getDiagonal() << endl;
+    cout << "CPU Gen" << number_1.getCPUgeneration() << endl;
+    cout << "CPU speed:" << number_1.getCPUgeneration() << endl;
+    cout << "CPU speed:" << number_1.getSpeedCPU() << endl << "\n";
+    cout << "Model:" << number_3.getModel() << endl;
+    cout << "OS:" << number_3.getOS() << endl;
+    cout << "Firm:" << number_3.getNameOfFirm() << endl;
+    cout << "RAM:" << number_3.getStorageRAM() << endl;
+    cout << "Diagonal:" << number_3.getDiagonal() << endl;
+    cout << "CPU Gen:" << number_3.getCPUgeneration() << endl;
+    cout << "Cpu Speed:" << number_3.getCPUgeneration() << endl;
+    cout << "Cpu speed:" << number_3.getSpeedCPU() << endl << "\n";
     return 0;
-
 }
